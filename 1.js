41dbaa4b4af39f1cbdaed3003f7bc38af368f94d
Daniel Azevedo <azevedo_daniel1994@hotmail.com>
2021-11-24 17:27:44 +0000

    refactor

--- 1.js
+++ 1.js
@@ -1,53 +1,78 @@
 'use strict'
 
 const fs = require('fs')
-let ocurrences = []
-let size
-let symbols = 0 
+let totalOcurrences = 0
 
 function getBuffer(filename) {
     const data  = fs.readFileSync(`./${filename}`)
-    size = data.length
+    totalOcurrences = data.length
     return data
 }
 
 function processFile(content) {
-    content.forEach(value => {
-        let check = ocurrences[value]
-        if (check == undefined) {
-            symbols++
-            ocurrences[value] = 1
+    let ocurrences = {}
+    content.forEach(byte => {
+        let char = String.fromCharCode(byte)
+        if (ocurrences[char] != undefined) {
+            ocurrences[char] += 1
         } else {
-            ocurrences[value] += 1
+            ocurrences[char] = 1
         }
     })
-    console.log(ocurrences)
+    return ocurrences
 }
 
-function probabiltyExtraction () {
-    console.log(size)
-    return ocurrences.map(elem => {
-        return elem/size
+function sortObject(obj) {
+    var arr = []
+    for (var prop in obj) {
+        if (obj.hasOwnProperty(prop)) {
+            arr.push({
+                'key': prop,
+                'value': obj[prop]
+            })
+        }
+    }
+    arr.sort(function(a, b) { return b.value - a.value; })
+    return arr
+}
+
+function probabiltyExtraction (arr) {
+    return arr.map(elem => {
+        return elem.value/totalOcurrences
     })
 }
 
-function entropyCalculator () {
+function entropyCalculator (arr) {
     let entropy = 0
-    probabilityArray.forEach(prob => {
+    arr.forEach(prob => {
         entropy += prob * Math.log2(1/prob)
     })
     return entropy
 }
 
+function getSymbolsWithHigherProb (arr) {
+    let sum = 0
+    let nSymbols = 0
+    for (let key in arr) {
+        if (sum >= 0.5)
+            break
+        sum += arr[key]
+        nSymbols++
+    }
+    return nSymbols 
+}
+
+
+
 //1a
-let res = getBuffer("pdf.pdf")
-processFile(res)
-let probabilityArray = probabiltyExtraction()
-let H = entropyCalculator()
-console.log(H)
+let fileBytes = getBuffer("pdf.pdf")
+let ocurrences = processFile(fileBytes)
+let sortedOcurrences = sortObject(ocurrences)
+console.log(sortedOcurrences)
+let probabilityArray = probabiltyExtraction(sortedOcurrences)
+let entropy = entropyCalculator(probabilityArray)
+console.log('H = ' + entropy)
 
 //1b
-console.log(symbols)
-
-
-
+let nSymbols = getSymbolsWithHigherProb(probabilityArray)
+console.log('nSymbTo to get 0,5 or more prob = ' + nSymbols)
