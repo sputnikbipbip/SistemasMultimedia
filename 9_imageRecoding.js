41706aa660e5105c5e42293a5d1cf62bc0acf979
Daniel Azevedo <azevedo_daniel1994@hotmail.com>
2022-01-22 14:36:19 +0000

    new test files, 9 bug fix and refactor

--- 9_imageRecoding.js
+++ 9_imageRecoding.js
@@ -1,5 +1,10 @@
 'use strict'
 
+
+const path = require('path');
+const fs = require('fs'),
+    PNG = require('pngjs').PNG;
+
 let I1k = []
 let I1i = []
 let I1j = []
@@ -9,11 +14,18 @@ let I2j = []
 let I1_I2 = []
 
 
-const fs = require('fs'),
-PNG = require('pngjs').PNG;
-
-function getMSE() {
-    var data = fs.readFileSync('./png_photos/kodim23original.png');
+/**
+ * filePath1 must be the original png in order to calculate the e256 correctly
+ * 
+ * @param {string} filePath1 
+ * @param {string} filePath2 
+ * @returns 
+ */
+function getMSE(filePath1, filePath2) {
+    var data = fs.readFileSync(filePath1, function(err, data){
+        if(err) console.log(err)
+        return data
+      });;
     var png = PNG.sync.read(data);
     for (var y = 0; y < png.height; y++) {
         for (var x = 0; x < png.width; x++) {
@@ -23,7 +35,10 @@ function getMSE() {
           I1j.push(png.data[idx + 2])
         }
     }
-    data = fs.readFileSync('./png_photos/kodim23uploaded.png');
+    data = fs.readFileSync(filePath2, function(err, data){
+        if(err) console.log(err)
+        return data
+      });
     png = PNG.sync.read(data);
     for (var y = 0; y < png.height; y++) {
         for (var x = 0; x < png.width; x++) {
@@ -31,14 +46,21 @@ function getMSE() {
           I2k.push(png.data[idx])
           I2i.push(png.data[idx + 1])
           I2j.push(png.data[idx + 2])
+          //e256 - updating current png to become e256 image file (original file - compressed file)
           png.data[idx] = (I1k[idx] - png.data[idx]) % 256 
           png.data[idx + 1] = (I1i[idx + 1] - png.data[idx + 1]) % 256
           png.data[idx + 2] = (I1j[idx] - I1j[idx + 2]) % 256
         }
     }
     /**E256 image*/
-    var buffer = PNG.sync.write(png);
-    fs.writeFileSync('./E256/kodim23_e256.png', buffer);
+    let buffer = PNG.sync.write(png);
+    let name = path.basename(filePath1).split('.').slice(0, -1)
+    fs.writeFileSync(`./E256/${name}_e256.png`, buffer, err => {
+        if (err) {
+            console.error(err)
+            return
+        }
+      })
 
     for (let i = 0; i < I1k.length; i++) {
         I1_I2.push((I1i[i] - I2i[i]) + (I1j[i] - I2j[i]) + (I1k[i] - I2k[i]))
@@ -51,11 +73,23 @@ function getMSE() {
     return Somatory / N
 }
 
+/**
+ * 
+ * @param {Number} MSE 
+ * @returns {string} PSNR
+ */
 function getPSNR(MSE) {
     return (10 * Math.log10((Math.pow(255, 2)) / MSE)) + ' dB'
 }
 
-
-let MSE = getMSE()
+/**
+ * MSE and PSNR extraction,
+ * every time the methods runs it generates an e256 file made by (firstImage - secondImage)
+ */
+let MSE = getMSE('./png_photos/kodim23original.png', './png_photos/kodim23uploaded.png')
 let PSNR = getPSNR(MSE)
 console.log(`MSE = ${MSE} \nPSNR = ${PSNR}`)
+
+/**
+ * get original pgn file by adding e256 from (original_WebP - loss_WebP)
+ */
